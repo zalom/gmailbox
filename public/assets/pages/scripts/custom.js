db38c037d58da3491e052e3164c174af56441ce6
Zlatko Alomerovic <zlatko@miavision.net>
2016-07-12 11:40:25 +0200

    Improved static html as requested

--- public/assets/pages/scripts/custom.js
+++ public/assets/pages/scripts/custom.js
@@ -1,21 +1,29 @@
-$('button.reply-btn').on('click',function(e) {
-  e.preventDefault();
-  window.location = "http://localhost:3000/mailbox/reply.html";
-});
-$('button#back-btn, button#register-back-btn').on('click', function(e) {
-  e.preventDefault();
-  window.location = "http://localhost:3000/mailbox/login.html";
-});
+$(document).ready(function(){
+  $('button.reply-btn').on('click',function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/reply.html";
+  });
+  $('button#back-btn, button#register-back-btn').on('click', function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/login.html";
+  });
 
-$('button#login-btn, button#register-submit-btn, button#forgot-submit').on('click', function(e) {
-  e.preventDefault();
-  window.location = "http://localhost:3000/mailbox/inbox.html";
-});
-$('button#btn-send, button#btn-discard').on('click',function(e) {
-  e.preventDefault();
-  window.location = "http://localhost:3000/mailbox/inbox.html";
-});
-$('button#btn-draft').on('click',function(e) {
-  e.preventDefault();
-  window.location = "http://localhost:3000/mailbox/draft.html";
+  $('button#login-btn, button#register-submit-btn, button#forgot-submit').on('click', function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/inbox.html";
+  });
+  $('button#btn-send, button#btn-discard').on('click',function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/inbox.html";
+  });
+  $('button#btn-draft').on('click',function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/draft.html";
+  });
+
+  $("a.thread-link").click(function(){
+    $('div.thread').load('thread.html');
+     //alert("Thanks for visiting!");
+  });
+
 });
