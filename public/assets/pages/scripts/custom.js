418e7dd1ca0d38c5f2445b9888e42492dc59c7e1
Zlatko Alomerovic <zlatko@miavision.net>
2016-07-27 13:29:05 +0200

    Markup and styles additions

--- public/assets/pages/scripts/custom.js
+++ public/assets/pages/scripts/custom.js
@@ -3,16 +3,12 @@ $(document).ready(function(){
     e.preventDefault();
     window.location = "http://localhost:3000/mailbox/reply.html";
   });
-  $('button#back-btn, button#register-back-btn').on('click', function(e) {
+  $('button#back-btn, button#register-back-btn, button#change-password-btn').on('click', function(e) {
     e.preventDefault();
     window.location = "http://localhost:3000/mailbox/login.html";
   });
 
-  $('button#login-btn, button#register-submit-btn, button#forgot-submit').on('click', function(e) {
-    e.preventDefault();
-    window.location = "http://localhost:3000/mailbox/inbox.html";
-  });
-  $('button#btn-send, button#btn-discard').on('click',function(e) {
+  $('button#login-btn, button#register-submit-btn, button#forgot-submit, button#btn-send, button#btn-discard').on('click', function(e) {
     e.preventDefault();
     window.location = "http://localhost:3000/mailbox/inbox.html";
   });
@@ -20,10 +16,14 @@ $(document).ready(function(){
     e.preventDefault();
     window.location = "http://localhost:3000/mailbox/draft.html";
   });
-
+  $('button#forgot-submit').on('click',function(e) {
+    e.preventDefault();
+    window.location = "http://localhost:3000/mailbox/reset_password.html";
+  });
   $("a.thread-link").click(function(){
     $('div.thread').load('thread.html');
      //alert("Thanks for visiting!");
   });
+  
 
 });
