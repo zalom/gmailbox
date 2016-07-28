9dfa7e959ad842a84a905051b374f6d9b23abbee
Zlatko Alomerovic <zlatko@miavision.net>
2016-07-28 17:08:06 +0200

    Removed Devise controllers (for now)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    #redirect_to '/mailbox/login.html'
+    #redirect_to '/mailbo/xlogin.html'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
-  devise_for :users, controllers: { registrations: 'registrations',
-                                    sessions: 'users/sessions' }
+  devise_for :users, path: "", controllers: { registrations: 'registrations' }
 
   resource :profile, only: [:show, :edit, :update]
 end
