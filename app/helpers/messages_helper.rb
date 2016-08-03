7a66d90aba249fb9e87e68958557af8f344d0776
Zlatko Alomerovic <zlatko@miavision.net>
2016-08-03 14:06:43 +0200

    Refactoring seeds and helper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,11 +2,11 @@ module MessagesHelper
   def inbox_sent_format(sent_at)
     case sent_at
     when sent_at.year < Time.now.year
-      sent_at.strftime("%d/%m/%y")
+      sent_at.strftime('%d/%m/%y')
     when sent_at.month <= Time.now.month && sent_at.day < Time.now.day
-      sent_at.strftime("%B %d")
+      sent_at.strftime('%B %d')
     else
-      sent_at.strftime("%H:%M")
+      sent_at.strftime('%H:%M')
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 users = {
   user1: { email: 'zlatko@example.com', password: '123456' },
-  user2: { email: 'ramo@example.com', password: '123456' },
-  user3: { email: 'semso@example.com', password: '123456' }
+  user2: { email: 'mirza@example.com', password: '123456' },
+  user3: { email: 'ervin@example.com', password: '123456' },
+  user4: { email: 'sedad@example.com', password: '123456' },
+  user5: { email: 'enes@example.com', password: '123456' },
+  user6: { email: 'jasmin@example.com', password: '123456' }
 }
 
 users.each do |_k, v|
@@ -27,21 +23,21 @@ user3 = User.find_by_email(users[:user3][:email])
 user1.sent_messages.create(
   subject: 'First email ever',
   content: 'First seeded email',
-  recipient_id: User.find_by_email('semso@example.com').id,
+  recipient_id: User.find_by_email(users[:user3][:email]).id,
   sent_at: Time.now
 )
 
 user2.sent_messages.create(
   subject: 'Second email',
   content: 'Second seeded email',
-  recipient_id: User.find_by_email('semso@example.com').id,
+  recipient_id: User.find_by_email(users[:user3][:email]).id,
   sent_at: Time.now
 )
 
 user3.sent_messages.create(
   subject: 'First email ever',
   content: 'I am answering to the First seeded email',
-  recipient_id: User.find_by_email('zlatko@example.com').id,
+  recipient_id: User.find_by_email(users[:user1][:email]).id,
   sent_at: Time.now,
   thread_id: 1
 )
@@ -49,7 +45,7 @@ user3.sent_messages.create(
 user3.sent_messages.create(
   subject: 'Second email',
   content: 'I am answering to the Second seeded email',
-  recipient_id: User.find_by_email('ramo@example.com').id,
+  recipient_id: User.find_by_email(users[:user2][:email]).id,
   sent_at: Time.now,
   thread_id: 2
 )
