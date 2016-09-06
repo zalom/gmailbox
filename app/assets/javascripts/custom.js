697d34aba1041bd799b3efc6d432e3fbfb1193cc
Zlatko Alomerovic <zlatko@miavision.net>
2016-09-06 15:18:55 +0200

    Bugfix: Check all checkboxes button was faulty

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,34 @@
 $(document).on('turbolinks:load', function() {
   $('#check_all').on('click', 'input[type=checkbox]', function() {
-    $('tbody').find('label.mt-checkbox > input[type=checkbox]').each(function() {
-      toggleCheckBox($(this));
-    });
+    //resetChecked();
+    if($(this).prop('checked') === 'checked' || $(this).prop('checked')){
+      checkAll();
+    }
+    else{
+      unCheckAll();
+    }
+  });
+  $('.single-label').on('click', 'input[type=checkbox]', function() {
+    $('#check_all > input[type=checkbox]').prop('checked', false);
   });
 });
 
-function toggleCheckBox(et){
-  et.prop('checked') === 'checked' || et.prop('checked') ? et.prop('checked', false) : et.prop('checked', true);
+function checkAll(){
+  $('tbody').find('label.single-label > input[type=checkbox]').each(function() {
+    $(this).prop('checked', true);
+  });
 }
+
+function unCheckAll(){
+  $('tbody').find('label.single-label > input[type=checkbox]').each(function() {
+    $(this).prop('checked', false);
+  });
+}
+
+/*
+
+$('tbody').find('label.mt-checkbox > input[type=checkbox]').each(function() {
+  toggleCheckBox($(this));
+});
+
+*/
